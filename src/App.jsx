3edc1ce0f59e402e4ffe01864aa412d1a0c32114
RyanFernando27 <138043611+RyanFernando27@users.noreply.github.com>
2025-03-07 21:20:53 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from 'react';
 import { BounceLoader } from 'react-spinners';
 import MovieCard from './components/MovieCard'; // Ensure this path is correct
 import Pagination from './components/Pagination';
-import './App.css'; // Import the CSS file
+
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
